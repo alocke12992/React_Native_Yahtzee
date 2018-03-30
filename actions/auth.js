2f7fe39fef4b0fedeabe60fc686fce094ede683a
Andrew Locke <alocke12992@gmail.com>
2018-03-30 11:19:45 -0600

    Adds auth routes

--- actions/auth.js
+++ actions/auth.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
-
+import {AUTH_URL} from '../utils/urls';
 export const LOGIN = 'LOGIN';
 export const LOGOUT = 'LOGOUT';
 export const VALIDATE_TOKEN = 'VALIDATE_TOKEN';
 
 export const login = (email, password, history) => {
   return (dispatch) => {
-    axios.post('/auth/sign_in', { email, password })
-      .then(({ data, headers }) => {
+    axios.post(`${AUTH_URL}/sign_in`, {email, password})
+      .then(({data, headers}) => {
         dispatch(user(data, headers))
         history.push('/')
       })
@@ -21,8 +21,8 @@ export const register = (
   history
 ) => {
   return (dispatch) => {
-    axios.post('/auth', { email, password, password_confirmation })
-      .then(({ data, headers }) => {
+    axios.post(`${AUTH_URL}`, {email, password, password_confirmation})
+      .then(({data, headers}) => {
         dispatch(user(data, headers))
         history.push('/')
       })
@@ -31,17 +31,17 @@ export const register = (
 
 export const logout = (history) => {
   return (dispatch) => {
-    axios.delete('/auth/sign_out')
-      .then(res => dispatch({ type: LOGOUT }))
+    axios.delete(`${AUTH_URL}/sign_out`)
+      .then(res => dispatch({type: LOGOUT}))
   }
 }
 
-export const validateToken = (cb = () => { }) => {
+export const validateToken = (cb = () => {}) => {
   return (dispatch) => {
-    dispatch({ type: VALIDATE_TOKEN })
+    dispatch({type: VALIDATE_TOKEN})
     const headers = axios.defaults.headers.common
-    axios.get('/auth/validate_token', headers)
-      .then(({ data, headers }) => {
+    axios.get(`${AUTH_URL}/validate_token`, headers)
+      .then(({data, headers}) => {
         dispatch(user(data, headers))
         cb()
       }).catch(() => cb())
@@ -49,5 +49,5 @@ export const validateToken = (cb = () => { }) => {
 }
 
 const user = (res, headers) => {
-  return { type: LOGIN, user: res.data, headers }
+  return {type: LOGIN, user: res.data, headers}
 }
